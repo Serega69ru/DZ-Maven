c972b04eaa51a360cbda8de56e93c4f32229d8e4
Sergey Ivanov <ivanovsa69@yandex.ru>
2023-10-19 00:31:36 +0300

    second commit

--- src/main/java/ru/netology/javaqa/Main.java
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -2,6 +2,9 @@ package ru.netology.javaqa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BonusService service = new BonusService();
+        long amount = 10000;
+        long bonus = service.calculate(amount); // должно получиться 500
+        System.out.println(bonus);
     }
 }
